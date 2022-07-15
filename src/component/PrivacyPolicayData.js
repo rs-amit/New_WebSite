d93c32939c76e5338ccbcaf824025d2bb04be9c9
Amit Mishra <amitrs852852@gmail.com>
2022-07-15 15:58:57 +0530

    initial commit

---
+++ src/component/PrivacyPolicayData.js
@@ -0,0 +1,101 @@
+
+export const ParaGraphArray = [
+    'Pets Centric Private Limited a private limited company with its registered office at 112, 11th Cross, 4th Sector, HSR Layout, Bangalore, Karnataka, India, 560102 operates the website www.supertails.com (hereinafter referred to as the "Platform"). The Company is committed to protecting Your privacy and the information that You share while using the Platform. We value the trust You place in Us. That’s why We maintain the highest security standards for securing the transactions and Your information.'
+    ,
+    'This privacy policy specifies the manner in which personal data and other information is collected, received, stored, processed, disclosed, transferred, dealt with, or otherwise handled by the Company. This Privacy Policy does not apply to information that You provide to, or that is collected by, any third-party through the Platform, and any Third-Party Sites (defined below) that You access or use in connection with the Services offered on the Platform.'
+    ,
+    'Please read the Privacy Policy carefully prior to using or registering on the Platform or accessing any material, information or availing any Services through the Platform.'
+    ,
+    'Please read the Privacy Policy carefully prior to using or registering on the Platform or accessing any material, information or availing any Services through the Platform.'
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const collectionOfInformation = [{
+    title: 'Collection of Information',
+    basicInfo: '',
+    infoPoints: [
+        `You may provide or the Company may collect certain personal information while You register on the Platform and/or use the Services. This includes: (a) Your name, electronic mail address, password, IP address, billing address, shipping address and other relevant details; (b) Your content, received, uploaded or posted using the Platform, including messages, images, videos and feedback; (c) any information provide by You while availing customer support, and (d) information You choose to upload, sync or import from the device on which the Platform is being used `,
+        `The Company may collect non-personal information relating to Your activities while accessing the Service or other information from and about the devices through which the Platform is used including SDK/API/JS code version, browser, internet service provider, operating system, browser type, cookie information, timestamp, application identifier, application version, application distribution channel, independent device identifier, Android ad master identifier, network card (MAC) address, and international mobile device identification code (IMEI)`
+    ],
+    ExtraInfo: '',
+},
+{
+    title: 'Usage of Information',
+    basicInfo: 'The information as specified in Clause 1, may be used by the Company for the following purposes:',
+    infoPoints: [
+        "for Your registration, login, and management of account on the Platform",
+        "to confirm Your identity directly and/or through third parties;",
+        "to provide You Services and improve the Services",
+        "remembering Your Personal Information so that You are not required to re-enter it the next time You visit the Platform",
+        "to understand Your preferences and to enhance and customize Your experience of using the Service and the Platform",
+    ],
+    ExtraInfo: `The data gathered from You may also be used for any reason incidental to the reasons listed above; and for any other purpose with Your consent. We use Your Personal Information to send You promotional emails, however, We will provide You the ability to opt-out of receiving such emails from Us. However, You will not be able to opt-out of receiving administrative messages, customer service responses or other transactional communications. Unless and until, You explicitly give Your consent to Us, to do so, We will not share Your Personal Information with another user of the Platform and vice versa.`
+}
+]
+
+
+
+
+
+
+
+
+
+
+export const policyContent = [{
+    title: 'Cookies',
+    content: ['A cookie is a small amount of information that’s downloaded to Your computer or device when You visit the Platform. We use a number of different cookies, including functional, performance, advertising, and social media or content cookies. Cookies make Your browsing experience better by allowing the Platform to remember Your actions and preferences (such as login and region selection), and Your trends. This means You will not have to re-enter this information each time You return to the Platform or browse from one page to another.',
+        'The length of time that a cookie remains on Your computer or mobile device depends on whether it is a “persistent” or “session” cookie. Session cookies last until You stop browsing and persistent cookies last until they expire or are deleted. Most of the cookies We use are persistent and will expire between 30 minutes and two years from the date they are downloaded to Your device.']
+},
+{
+    title: 'Disclosure to Third Parties',
+    content: ['A cookie is a small amount of information that’s downloaded to Your computer or device when You visit the Platform. We use a number of different cookies, including functional, performance, advertising, and social media or content cookies. Cookies make Your browsing experience better by allowing the Platform to remember Your actions and preferences (such as login and region selection), and Your trends. This means You will not have to re-enter this information each time You return to the Platform or browse from one page to another.',
+        'The length of time that a cookie remains on Your computer or mobile device depends on whether it is a “persistent” or “session” cookie. Session cookies last until You stop browsing and persistent cookies last until they expire or are deleted. Most of the cookies We use are persistent and will expire between 30 minutes and two years from the date they are downloaded to Your device.']
+},
+{
+    title: 'Policy Towards Children',
+    information: 'The Platform is intended for a general audience and not for use by anyone younger than the age of 18. In the event the Platform and the Services are being accessed by a person below the age of 18 (eighteen) years (“Minor”), such access shall be deemed to be with the consent of the guardian of such Minor.  The Company does not knowingly collect personal information from children younger than the age of 18 without the consent of a parent or legal guardian; if the Company learns that it has done so, the Company will promptly remove the information from all active databases. If You are a parent or legal guardian who believes that the Company has collected or used their child’s personal information, please contact the Company at support@supertails.com.',
+    content: []
+},
+{
+    title: 'Security',
+    information: '',
+    content: ['A cookie is a small amount of information that’s downloaded to Your computer or device when You visit the Platform. We use a number of different cookies, including functional, performance, advertising, and social media or content cookies. Cookies make Your browsing experience better by allowing the Platform to remember Your actions and preferences (such as login and region selection), and Your trends. This means You will not have to re-enter this information each time You return to the Platform or browse from one page to another.',
+        'The length of time that a cookie remains on Your computer or mobile device depends on whether it is a “persistent” or “session” cookie. Session cookies last until You stop browsing and persistent cookies last until they expire or are deleted. Most of the cookies We use are persistent and will expire between 30 minutes and two years from the date they are downloaded to Your device.']
+},
+
+
+
+]
+
+
+export const BasicInfo = [{
+    title: "Grievances",
+    content: [
+        'In the event You have any grievances relating to the Privacy Policy, please inform the Company within 24 hours of occurrence of the instance from which the grievance has arisen, by writing an email to the Grievance Redressal Officer as follows',
+        'Name of the Grievance Redressal Officer: Harsh Chandak',
+        'Contact information: harshlovespets@supertails.com'
+    ]
+},
+{
+    title: "Amendments",
+    content: [
+        'Name of the Grievance Redressal Officer: Harsh Chandak',
+        'Contact information: harshlovespets@supertails.com'
+    ]
+}
+]
+
+
--- src/screens/PrivacyPolicy.js
+++ src/screens/PrivacyPolicy.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { styled } from "@mui/material/styles";
+import { ParaGraphArray, collectionOfInformation, policyContent,  BasicInfo   } from "../component/PrivacyPolicayData"
 
 
 const Container = styled('div')(({ theme }) => ({
@@ -36,39 +37,6 @@ const ParaGraph = styled('p')(({ theme }) => ({
 }))
 
 
-const ParaGraphArray = [
-   'Pets Centric Private Limited a private limited company with its registered office at 112, 11th Cross, 4th Sector, HSR Layout, Bangalore, Karnataka, India, 560102 operates the website www.supertails.com (hereinafter referred to as the "Platform"). The Company is committed to protecting Your privacy and the information that You share while using the Platform. We value the trust You place in Us. That’s why We maintain the highest security standards for securing the transactions and Your information.'
-, 
-   'This privacy policy specifies the manner in which personal data and other information is collected, received, stored, processed, disclosed, transferred, dealt with, or otherwise handled by the Company. This Privacy Policy does not apply to information that You provide to, or that is collected by, any third-party through the Platform, and any Third-Party Sites (defined below) that You access or use in connection with the Services offered on the Platform.'
-, 
-   'Please read the Privacy Policy carefully prior to using or registering on the Platform or accessing any material, information or availing any Services through the Platform.'
-, 
-   'Please read the Privacy Policy carefully prior to using or registering on the Platform or accessing any material, information or availing any Services through the Platform.'
-]
-
-const collectionOfInformation = [{
-    title: 'Collection of Information',
-    basicInfo: '',
-    infoPoints: [
-        `You may provide or the Company may collect certain personal information while You register on the Platform and/or use the Services. This includes: (a) Your name, electronic mail address, password, IP address, billing address, shipping address and other relevant details; (b) Your content, received, uploaded or posted using the Platform, including messages, images, videos and feedback; (c) any information provide by You while availing customer support, and (d) information You choose to upload, sync or import from the device on which the Platform is being used `,
-        `The Company may collect non-personal information relating to Your activities while accessing the Service or other information from and about the devices through which the Platform is used including SDK/API/JS code version, browser, internet service provider, operating system, browser type, cookie information, timestamp, application identifier, application version, application distribution channel, independent device identifier, Android ad master identifier, network card (MAC) address, and international mobile device identification code (IMEI)`
-    ],
-    ExtraInfo: '',
-},
-{
-    title: 'Usage of Information',
-    basicInfo: 'The information as specified in Clause 1, may be used by the Company for the following purposes:',
-    infoPoints: [
-        "for Your registration, login, and management of account on the Platform",
-        "to confirm Your identity directly and/or through third parties;",
-        "to provide You Services and improve the Services",
-        "remembering Your Personal Information so that You are not required to re-enter it the next time You visit the Platform",
-        "to understand Your preferences and to enhance and customize Your experience of using the Service and the Platform",
-    ],
-    ExtraInfo: `The data gathered from You may also be used for any reason incidental to the reasons listed above; and for any other purpose with Your consent. We use Your Personal Information to send You promotional emails, however, We will provide You the ability to opt-out of receiving such emails from Us. However, You will not be able to opt-out of receiving administrative messages, customer service responses or other transactional communications. Unless and until, You explicitly give Your consent to Us, to do so, We will not share Your Personal Information with another user of the Platform and vice versa.`
-}
-]
-
 
 
 function PrivacyPolicy() {
@@ -87,29 +55,62 @@ function PrivacyPolicy() {
                                     <ParaGraph>
                                         {content}
                                     </ParaGraph>
-                                    
+
                                 </>
                             </>
                         ))
                     }
-                    <>
-                        {collectionOfInformation.map((data, index) => (
-                            <div key={index}>
-                                <ol>
-                                    <InfoList>{data.title}</InfoList>
-                                </ol>
-                                {data.basicInfo && <ParaGraph>{data.basicInfo}</ParaGraph>}
-                                <ul>
-                                    {
-                                        data.infoPoints.map((item, index) => (
-                                            <InfoList key={index}>{item}</InfoList>
-                                        ))
-                                    }
-                                </ul>
-                            </div>
-                        ))
-                        }
-                    </>
+
+                    {collectionOfInformation.map((data, index) => (
+                        <div key={index}>
+                            <ol>
+                                <InfoList><strong>{data.title}</strong></InfoList>
+                            </ol>
+                            {data.basicInfo && <ParaGraph>{data.basicInfo}</ParaGraph>}
+                            <ul>
+                                {
+                                    data.infoPoints.map((item, index) => (
+                                        <InfoList key={index}>{item}</InfoList>
+                                    ))
+                                }
+                            </ul>
+                            {data.basicInfo && <ParaGraph>{data.ExtraInfo}</ParaGraph>}
+
+                        </div>
+                    ))
+                    }
+
+                    {policyContent.map((data,index)=>(
+                        <div key={index}>
+                            <ol>
+                                <InfoList><strong>{data.title}</strong></InfoList>
+                            </ol>
+                            {data.content && (
+                            <ul>
+                                <InfoList>{data.content}</InfoList>
+                            </ul>)
+                            }
+                            {data.information && <ParaGraph>{data.information}</ParaGraph>}
+                            
+                        </div>
+                    ))
+
+                    }
+
+                    {
+                       BasicInfo.map((data,index)=>(
+                        <div key={index}>
+                            <ol>
+                              <InfoList><strong>{data.title}</strong></InfoList>
+                            </ol>
+                          {data.content.map((item, index )=>(
+                             <ParaGraph key={index}>{item}</ParaGraph>
+                          ))}
+                            
+                        </div>
+                       ))
+                    }
+
                 </div>
 
             </View>
@@ -117,4 +118,4 @@ function PrivacyPolicy() {
     )
 }
 
-export default PrivacyPolicy ;
+export default PrivacyPolicy;
